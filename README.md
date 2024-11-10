7a63b2c12bad2c4847ce34ce31c991d348860b3e
jme14 <williamsltrey3@gmail.com>
2024-11-10 13:04:10 -0600

    move to antlr4ts rather than antlr

--- README.md
+++ README.md
@@ -3,10 +3,6 @@
 2) nodejs 
 3) java
 
-## Setup
+## Setup 
 1) `git clone (this repo)`
-2) `npm install` 
-    (this reads the package.json file and downloads the proper npm modules)
-3) `npm install --save-dev @types/node` (adds in definitions for node)
-4) `antlr4 -Dlanguage=TypeScript -o generated TMNH.g4` OR `npm run build`
-5) `npm run start` OR `tsc parseFiles.ts && node parseFiles.js`
+2) `npm run rerun` (this will also install necessary node modules)
--- parseFiles.ts
+++ parseFiles.ts
@@ -1,13 +1,14 @@
-import * as fs from 'fs';
-import { CharStreams, CommonTokenStream } from 'antlr4';
-import TMNHLexer from './generated/TMNHLexer';
-import TMNHParser from './generated/TMNHParser';
-import TMNHListener from './generated/TMNHListener'; 
+import * as fs from "fs";
+import { CharStreams, CommonTokenStream } from "antlr4ts";
+import { TMNHLexer } from "./generated/TMNHLexer";
+import { TMNHParser } from "./generated/TMNHParser";
 
-const input = fs.readFileSync('project_deliverable_1.py', 'utf-8');
-const lexer = new TMNHLexer(CharStreams.fromString(input));
+const input = fs.readFileSync("project_deliverable_1.py", "utf-8");
+const charStream = CharStreams.fromString(input)
+const lexer = new TMNHLexer(charStream);
 const tokenStream = new CommonTokenStream(lexer);
 const parser = new TMNHParser(tokenStream);
 const tree = parser.prog();
 
-console.log(tree); //IDK if this will work im just yoloing
+console.log(tree.toStringTree(parser))
+
